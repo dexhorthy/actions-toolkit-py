708f31b340db23915102077afae6aa417ed93189
dexhorthy <dexter@replicated.com>
2022-04-08 21:03:27 -0500

    can we get the event into the action?

--- actions_toolkit/github_project.py
+++ actions_toolkit/github_project.py
@@ -1,10 +1,6 @@
-import logging
-
-logger = logging.getLogger(__file__)
-
 
 def move_card_to_column(owner, repo, issue_content_url, project, column):
-    logger.info(
-        "moving card to column: %s",
+    print(
+        "moving card to column: %s" %
         " | ".join([owner, repo, issue_content_url, project, column]),
     )
---
+++ actions_toolkit/github_user.py
@@ -0,0 +1,9 @@
+from github import Github
+def get_member_status(org, user):
+    """
+    :param org:
+    :param member:
+    :return: True if the user is a public member of the org
+    """
+    # todo finish me
+    return True
